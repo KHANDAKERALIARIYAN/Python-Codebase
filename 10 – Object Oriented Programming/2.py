90fae527861dbf4fcec71439ba96e4b43055469a
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-06-06 01:39:12 +0600

    added chp 10

---
+++ 10 – Object Oriented Programming/2.py
@@ -0,0 +1,11 @@
+# instance vs class attribute
+
+class Employee: 
+    language = "Python" # This is a class attribute
+    salary = 1200000
+
+
+harry = Employee()
+harry.language = "JavaScript" # This is an instance attribute
+print(harry.language, harry.salary)
+ 
